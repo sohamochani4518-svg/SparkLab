e8ddeb5f38f065985878bdc7cfc4db99a80387aa
Soham Ochani <soham.ochani4518@gmail.com>
2026-08-10 20:50:07 +0530

    added proof of work

--- Arduino/Light_Regulator_Using_Potientiometer/Light_Regulator_Using_Potientiometer.ino
+++ Arduino/Light_Regulator_Using_Potientiometer/Light_Regulator_Using_Potientiometer.ino
@@ -1,17 +1,16 @@
-int readval ;
-float V2 ;
-int redpin = 9 ;
+int readval;
+float V2;
+int redpin = 9;
+
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
-pinMode(redpin,OUTPUT) ;
+  Serial.begin(9600);
+  pinMode(redpin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(redpin,HIGH);
-readval = analogRead(redpin);
-V2= (5./1023.)*readval ;
-Serial.println(V2) ;
-delay(750);
+  digitalWrite(redpin, HIGH);
+  readval = analogRead(A3);  
+  V2 = (5./ 1023.) * readval;
+  Serial.println(V2);
+  delay(750);
 }
